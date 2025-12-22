63ace451df677ec49c9bf9076b21b4c95577735e
zjun <565061763@qq.com>
2025-12-22 15:25:31 +0800

    test

--- app/src/test/java/com/sd/demo/debouncer/DebouncerTest.kt
+++ app/src/test/java/com/sd/demo/debouncer/DebouncerTest.kt
@@ -3,8 +3,8 @@ package com.sd.demo.debouncer
 import com.sd.lib.debouncer.Debouncer
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
@@ -14,66 +14,69 @@ class DebouncerTest {
   @Test
   fun `test success`() = runTest {
     val events = mutableListOf<String>()
+    val debouncer = Debouncer { events.add("onBlock") }
 
-    var debounceJob: Job? = null
-
-    val debouncer = Debouncer {
-      events.add("onBlock")
-      debounceJob?.cancel()
-    }
-
-    debounceJob = launch { debouncer.start(1000) { events.add("onStart") } }
+    launch { debouncer.start(1000) { events.add("onStart") } }
     advanceUntilIdle()
 
     debouncer.send()
+    advanceTimeBy(1001)
+    debouncer.cancel()
 
-    advanceUntilIdle()
     assertEquals(listOf("onStart", "onBlock"), events)
   }
 
   @Test
   fun `test success multi`() = runTest {
     val events = mutableListOf<String>()
+    val debouncer = Debouncer { events.add("onBlock") }
 
-    var debounceJob1: Job? = null
-    var debounceJob2: Job? = null
-
-    val debouncer = Debouncer {
-      events.add("onBlock")
-      debounceJob1?.cancel()
-      debounceJob2?.cancel()
-    }
-
-    debounceJob1 = launch { debouncer.start(1000) { events.add("onStart") } }
-    debounceJob2 = launch { debouncer.start(1000) { events.add("onStart") } }
+    launch { debouncer.start(1000) { events.add("onStart") } }
+    launch { debouncer.start(1000) { events.add("onStart") } }
     advanceUntilIdle()
 
     debouncer.send()
     debouncer.send()
+    advanceTimeBy(1001)
+    debouncer.cancel()
 
-    advanceUntilIdle()
     assertEquals(listOf("onStart", "onBlock"), events)
   }
 
   @Test
   fun `test send in onStart block`() = runTest {
     val events = mutableListOf<String>()
+    val debouncer = Debouncer { events.add("onBlock") }
 
-    var debounceJob: Job? = null
-
-    val debouncer = Debouncer {
-      events.add("onBlock")
-      debounceJob?.cancel()
-    }
-
-    debounceJob = launch {
+    launch {
       debouncer.start(1000) {
         events.add("onStart")
         debouncer.send()
       }
     }
+
+    advanceUntilIdle()
+    advanceTimeBy(1001)
+    debouncer.cancel()
+
+    assertEquals(listOf("onStart", "onBlock"), events)
+  }
+
+  @Test
+  fun `test cancel`() = runTest {
+    val events = mutableListOf<String>()
+    val debouncer = Debouncer { events.add("onBlock") }
+
+    launch { debouncer.start(1000) { events.add("onStart") } }
     advanceUntilIdle()
 
+    debouncer.send()
+    advanceTimeBy(1001)
+
+    debouncer.cancel()
+    debouncer.send()
+
+    advanceUntilIdle()
     assertEquals(listOf("onStart", "onBlock"), events)
   }
 }
